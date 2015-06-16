7ab3a32b7803b709281fb5af03d8b5a0cef1d7a7
jackbull <jackbull@qq.com>
2015-06-17 00:38:01 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var _config = {
     owner           : 'jackbull',           // github 用户名
     repo            : 'Issues',// github 中对应项目名
     duoshuo_id      : 'jackbullclouds',            // 在第三方评论插件多说申请站点的子域名
-    access_token : 'eb624e68280904899d5f64ed4722c94bc46a749c',                     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
+    // access_token : 'eb624e68280904899d5f64ed4722c94bc46a749c',                     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
     per_page        : '10'                    // 默认一页显示几篇文章
 }
 
